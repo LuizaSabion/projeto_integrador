c042367a717a152e9aea20e62ed6eb71b72207ed
nataly <tinarellinataly@gmail.com>
2022-09-30 11:38:11 -0300

    rodape

---
+++ includes/rodape.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <div class="card-body text-center">
+        <p>Copyright - <a href='http://www.ifsp.edu.br'
+                          target='_blank'>2022</a></p>
+    </div>
+</div>
